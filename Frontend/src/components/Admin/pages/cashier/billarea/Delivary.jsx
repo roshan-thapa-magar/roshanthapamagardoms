964d156d2c21cab6c4858a1eb8c1d29a3f4ee6dc
Roshan Thapa Magar <roshan@octspace.com>
2025-08-29 18:29:06 +0545

    hhh

--- Frontend/src/components/Admin/pages/cashier/billarea/Delivary.jsx
+++
@@ -1,6 +0,0 @@
-
-export default function Delivary() {
-  return (
-    <div>Delivary</div>
-  )
-}
--- Frontend/src/components/Admin/pages/cashier/billarea/TableBooking.jsx
+++
@@ -1,6 +0,0 @@
-
-export default function TableBooking() {
-  return (
-    <div>TableBooking</div>
-  )
-}
